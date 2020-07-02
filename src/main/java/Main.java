f869f99163adb9779aa1146945652017e958ec38
natalia.atanasii <n.atanasiy@gmail.com>
2020-07-02 17:20:42 +0300

    add HelperNpe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import syntax.HelperNPE;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi!");
+        //-XX:+ShowCodeDetailsInExceptionMessagesGoogle
+        HelperNPE.demo();
     }
+
 }
--- src/main/java/syntax/HelperNPE.java
+++ src/main/java/syntax/HelperNPE.java
@@ -1,4 +1,44 @@
 package syntax;
 
 public class HelperNPE {
+    public static void demo(){
+        var zoo = new Zoo();
+        try{
+            zoo.getDog().getAnimal().getType();
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
+
+    }
 }
+
+class Zoo {
+    private Dog dog = new Dog();
+
+    public Dog getDog(){
+        return dog;
+    }
+
+
+}
+class Dog {
+    private Animal animal;
+
+    public Animal getAnimal(){
+        return animal;
+    }
+
+}
+class Animal {
+    private String type;
+
+    public String getType(){
+        return type;
+    }
+}
+
+
+
+
+
+
