b00892eaebf235cf5fa33617a5acb4f5c85d55b9
natalia.atanasii <n.atanasiy@gmail.com>
2020-07-02 17:41:44 +0300

    add MuiltilineTextBlocks demo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
 import syntax.HelperNPE;
+import syntax.MultilineTextBlocks;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi!");
         //-XX:+ShowCodeDetailsInExceptionMessagesGoogle
-        HelperNPE.demo();
+        //HelperNPE.demo();
+        MultilineTextBlocks.demo();
     }
 
 }
--- src/main/java/syntax/MultilineTextBlocks.java
+++ src/main/java/syntax/MultilineTextBlocks.java
@@ -1,4 +1,28 @@
 package syntax;
 
-public class MultilinesBlocks {
+public class MultilineTextBlocks {
+
+        public static void demo() {
+            System.out.println(jdk8());
+            System.out.println(jdk14preview());
+        }
+
+        private static String jdk8() {
+            return "<html>\n" +
+                    "    <body>\n" +
+                    "        <p>TEST</p>\n" +
+                    "    </body>\n" +
+                    "</html>\n";
+        }
+
+        private static String jdk14preview() {
+            return """
+              <html>
+                  <body>
+                      <p>TEST>
+                  </body>
+              </html>
+              """;
+        }
+
 }
