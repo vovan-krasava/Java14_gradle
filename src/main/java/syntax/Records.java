d215a65e952a2ae0953e32ab14c455f638732df1
natalia.atanasii <n.atanasiy@gmail.com>
2020-07-10 22:32:32 +0300

    new class records

---
+++ src/main/java/syntax/Records.java
@@ -0,0 +1,15 @@
+package syntax;
+
+final class TriangleOld {
+    public final int x;
+    public final int y;
+    public final int z;
+
+    public TriangleOld(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    // equals, hashCode, toString
+}
+
